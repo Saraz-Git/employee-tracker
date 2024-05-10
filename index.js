9eaed4edffd0b57a1dd65829c656a9b047a3b742
Sara Zhou <sara@Saras-MacBook-Air.local>
2024-05-11 09:54:10 +1000

    plan file structure

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // Include packages needed for this application
+require('dotenv').config();
 const inquirer = require("inquirer");
+const { Pool } = require('pg');
 
+//Connect to database
+const pool = new Pool(
+    {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: 'localhost',
+        database: process.env.DB_NAME
+    },
+)
 //Create a function to initialize app
 const init = async () => { }
